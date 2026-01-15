175fb48cf187151386b7272e51fc6f5a406082a0
SorryTea <yaroslavkhomko@gmail.com>
2026-01-15 14:08:41 +0100

    Dodano szkielet transakcji finansowych

---
+++ agrobiznes/01_agro.Core/Economy/Transaction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_agro.Core.Economy
+{
+    public enum TransactionCategory
+    {
+        Energy,
+        Water,
+        Seeds,
+        Sales,
+        Fine,
+        Other
+    }
+    public enum TransactionType
+    {
+        Purchase,
+        Sale,
+        Penalty
+    }
+    public abstract class Transaction
+    {
+        public Guid Id { get; }
+        public DateTimeOffset OccurredAt { get; }
+        public Money Amount { get; }
+        public string Description { get; }
+        public TransactionCategory Category { get; }
+        public abstract TransactionType Type{ get; }
+
+
+    }
+}
