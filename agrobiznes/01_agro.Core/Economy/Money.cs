99aaed6faddb0a7219d51558d2c7412f97ce8077
SorryTea <yaroslavkhomko@gmail.com>
2026-01-15 13:25:50 +0100

    Dodano moduł Economy i obiekt Money

---
+++ agrobiznes/01_agro.Core/Economy/Money.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_agro.Core.Economy
+{
+    public readonly struct Money
+    {
+        public decimal Amount { get; }
+        public string Currency { get; }
+
+  
+        public Money(decimal amount, string currency)
+        {
+            if (amount < 0m)
+                throw new ArgumentOutOfRangeException(nameof(amount));
+
+            Amount = amount;
+            Currency = "PLN";
+        }
+        public override string ToString()
+        {
+            return $"{Amount:0.00} {Currency}";
+        }
+    }
+}
