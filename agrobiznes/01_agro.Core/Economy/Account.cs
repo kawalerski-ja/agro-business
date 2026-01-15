131fdd366420da99128d26a2b0045353094caecc
SorryTea <yaroslavkhomko@gmail.com>
2026-01-15 13:48:04 +0100

    Dodano klasę Account z delegatem BalanceChanged

---
+++ agrobiznes/01_agro.Core/Economy/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_agro.Core.Economy
+{
+    public class Account
+    {
+        public Money Balance { get; private set; }
+        public event Action<Money>? BalanceChanged;
+
+        public Account(Money initialBalance)
+        {
+            Balance = initialBalance;
+        }
+
+        public void Credit(Money amount)
+        {
+            Balance = new Money(Balance.Amount + amount.Amount, Balance.Currency);
+            BalanceChanged?.Invoke(Balance);
+        }
+        public void Debit(Money amount)
+        {
+            if (Balance.Amount > amount.Amount)
+            {
+                if (amount.Amount > Balance.Amount)
+                    throw new InvalidOperationException("Brak środków");
+
+                Balance = new Money(Balance.Amount - amount.Amount, Balance.Currency);
+                BalanceChanged?.Invoke(Balance);
+            }
+
+        }
+    }
+}
