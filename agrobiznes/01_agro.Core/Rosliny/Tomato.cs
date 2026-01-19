b93d120d91d90b595d23b805edeb321fb2329dd7
kawalerski-ja <kawalerski@student.agh.edu.pl>
2026-01-19 16:03:42 +0100

    Dodanie roślin dziedziczących

---
+++ agrobiznes/01_agro.Core/Rosliny/Tomato.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_agro.Core
+{
+    public class Tomato : Rosliny
+    {
+        public Tomato(): base("Pomidor",TypRosliny.Warzywo) { }
+        protected override void DoSpecificGrowth()
+        {
+            PoziomWzrostu += 2;
+        }
+    }
+}
