427eff4c76092ae618866f18d85ec70e7d5f0381
kawalerski-ja <kawalerski@student.agh.edu.pl>
2026-01-19 16:43:28 +0100

    Postawienie bazy na nowo po zmianach

---
+++ agrobiznes/02_agro.Data/Migrations/202601191542361_StructureReset.cs
@@ -0,0 +1,56 @@
+﻿namespace _02_agro.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class StructureReset : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Devices",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        Name = c.String(),
+                        IsOn = c.Boolean(nullable: false),
+                        Discriminator = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Roslinies",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        Nazwa = c.String(nullable: false),
+                        Typ = c.Int(nullable: false),
+                        Cena = c.Single(nullable: false),
+                        PoziomWzrostu = c.Single(nullable: false),
+                        PoziomNawodnienia = c.Single(nullable: false),
+                        PoziomNaslonecznienia = c.Single(nullable: false),
+                        IsDead = c.Boolean(nullable: false),
+                        Discriminator = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.SystemLogs",
+                c => new
+                    {
+                        LogId = c.Int(nullable: false, identity: true),
+                        Message = c.String(),
+                        Date = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.LogId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.SystemLogs");
+            DropTable("dbo.Roslinies");
+            DropTable("dbo.Devices");
+        }
+    }
+}
